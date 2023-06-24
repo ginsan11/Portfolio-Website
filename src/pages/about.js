d0debdf76a695daec28a28bfcd140776fdb1f9a9
ginsan11 <thezerodude2@gmail.com>
2023-06-24 17:54:22 -0600

    Update about.js
    fixed text issues on about page

--- src/pages/about.js
+++ src/pages/about.js
@@ -90,7 +90,7 @@ every project I work on. I look forward for the opportunity to be part of your t
                             <span className='inline-block text-7xl font-bold md:text-6xl sm:text-5xl xs:text-4xl'>
                             <AnimatedNumbers value={8} />+
                             </span>
-                            <h2 className='text-xl font-medium capitalize text-dark/75 dark:text-light/75
+                            <h2 className='text-xl font-small capitalize text-dark/75 dark:text-light/75
                             xl:text-center md:text-lg sm:text-base xs:text-sm'>Programming language experience</h2>
                         </div>
                     </div>
